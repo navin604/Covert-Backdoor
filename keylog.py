d7bd0b5440a2ac75bfe2bf2e2b460ed16a75061b
Navin Parmar <navinparmar98@gmail.com>
2023-06-20 16:41:33 -0700

    Fixed key press validations

--- keylog.py
+++ keylog.py
@@ -1,13 +1,12 @@
 import asyncio
-from evdev import InputDevice, ecodes
+from evdev import InputDevice, ecodes, categorize
 
 
 
 async def keylog(dev):
     async for event in dev.async_read_loop():
-        if event.type == ecodes.EV_KEY and event.code == 1:
-            print(repr(event))
-
+        if event.type == ecodes.EV_KEY and event.value == 1:
+            print(categorize(event))
 
 
 if __name__ == "__main__":
